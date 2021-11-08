91241622bd1c2a29ba080d59b5a1d9d5b90a11ed
ebtaterbug <ityjoseph@gmail.com>
2021-11-07 16:06:15 -0800

    add function to color code events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
-var events = [];
+var time = moment().hours();
 
-var currentDay = function() {
-    $("#currentDay").html(moment().format("dddd, MMMM Do"));
-}
-currentDay();
+$("#currentDay").html(moment().format("dddd, MMMM Do"));
+
+function checkTime() {
+    $(".time-block").each(function () {
+        var timeBlockId = parseInt($(this).attr("id"));
+        
+        console.log(this);
+
+        if (timeBlockId < time) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (timeBlockId === time) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).addClass("future");
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+        }
+    });
+};
+
+checkTime();
